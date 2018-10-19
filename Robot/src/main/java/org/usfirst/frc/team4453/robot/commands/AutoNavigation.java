b464eb68135cc4f5c5dbb58e8b613f0b92542915
Conner Ebbinghaus <connerebbinghaus@gmail.com>
2018-10-18 20:09:36 -0400

    Started work on Navigation Commands.

---
+++ Robot/src/main/java/org/usfirst/frc/team4453/robot/commands/AutoNavigation.java
@@ -0,0 +1,60 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team4453.robot.commands;
+
+import java.util.List;
+
+import org.usfirst.frc.team4453.robot.Robot;
+import org.usfirst.frc.team4453.robot.library.Navigation;
+import org.usfirst.frc.team4453.robot.library.Navigation.Coordinate;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class AutoNavigation extends Command {
+  
+  private List<Coordinate> coordinates;
+  private int currentCoordinate = 0;
+  private boolean isTurned = false;
+
+  public AutoNavigation(List<Coordinate> coordinates_in) {
+    requires(Robot.chassis);
+
+    coordinates = coordinates_in;
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    Coordinate goingTo = coordinates.get(currentCoordinate);
+    Coordinate currentCoordinate = Navigation.getCurrentCoordinate();
+
+    
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
--- Robot/src/main/java/org/usfirst/frc/team4453/robot/library/Navigation.java
+++ Robot/src/main/java/org/usfirst/frc/team4453/robot/library/Navigation.java
@@ -2,17 +2,34 @@ package org.usfirst.frc.team4453.robot.library;
 
 public class Navigation {
 
+    public static class Coordinate {
+        public double X;
+        public double Y;
+        public double A;
+
+        public Coordinate(double x_in, double y_in, double a_in) {
+            X = x_in;
+            Y = y_in;
+            A = a_in;
+        }
+    }
+
     public static double calculateDist(double x1, double y1, double x2, double y2){
-	double x = x1 - x2;
-	double y = y1 - y2;
-	double dist = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
-	return dist;
+    double x = x1 - x2;
+    double y = y1 - y2;
+    double dist = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+    return dist;
     }
     
     public static double calculateAngle (double x1, double y1, double x2, double y2){
-	double x = x2 - x1;
-	double y = y2 - y1;
-	double dangle = 90 - Math.toDegrees(Math.atan2(y, x));
-	return dangle;
+    double x = x2 - x1;
+    double y = y2 - y1;
+    double dangle = 90 - Math.toDegrees(Math.atan2(y, x));
+    return dangle;
+    }
+
+    public static Coordinate getCurrentCoordinate() {
+        // TODO
+        return new Coordinate(0, 0, 0);
     }
 }
